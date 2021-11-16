a88d47ccea54961c43fdcbbcd4133380caec3c10
katerina_topol <katerina_topol@mail.ru>
2021-11-16 19:36:12 +0300

    add task_1

--- task_1.py
+++ task_1.py
@@ -6,3 +6,21 @@
 Полученная строка выводится в главной функции.
 Элементы строки (элементы таблицы умножения) должны разделяться табуляцией.
 """
+
+list_to_str = lambda mult_table: '\t'.join([str(i) for i in mult_table])
+
+
+def table_multiplication(columns, line):
+    for i in range(line + 1)[1:]:
+        row = []
+        for j in range(columns + 1)[1:]:
+            if i == 0:
+                row.append(j)
+            elif j == 0:
+                row.append(i)
+            else:
+                row.append(i * j)
+        print(list_to_str(row))
+
+
+table_multiplication(3, 7)
