f8b68b6624fca3c37fa911e13344aee97d5e4f03
katerina_topol <katerina_topol@mail.ru>
2021-11-18 18:38:40 +0300

    add task_4

--- task_4.py
+++ task_4.py
@@ -10,3 +10,44 @@
 В функции необходимо проверять принадлежность суммы вклада к одному из диапазонов и выполнять расчет по нужной
 процентной ставке. Функция возвращает сумму вклада на конец срока.
 """
+
+PRODUCTS = (
+    {'begin_sum': 1000, 'end_sum': 10000, 6: 5, 12: 6, 24: 5},
+    {'begin_sum': 10000, 'end_sum': 100000, 6: 6, 12: 7, 24: 6.5},
+    {'begin_sum': 100000, 'end_sum': 1000000, 6: 7, 12: 8, 24: 7.5},
+)
+
+
+def check_valid_data(amount, term):
+    product = None
+    for i in PRODUCTS:
+        if i['begin_sum'] <= amount < i['end_sum'] and term in i.keys():
+            product = i
+    if product:
+        return True, product
+    else:
+        return False, None
+
+
+def calc_deposit(amount, term, product):
+    percent = product[term]
+    for month in range(term):
+        profit = amount * percent / 100 / 12
+        amount += profit
+
+    return round(amount, 2)
+
+
+if __name__ == '__main__':
+
+    while True:
+        amount = int(input('Введите сумму вклада: '))
+        term = int(input('Введите срок вклада: '))
+        check, product = check_valid_data(amount, term)
+        if not check:
+            print('Введены не корректные данные, попробуйте еще раз')
+        else:
+            break
+
+    sum_amount = calc_deposit(amount, term, product)
+    print(f'Сумма вклада на конец срока: {sum_amount}')
